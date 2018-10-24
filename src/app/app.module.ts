daebd53f1230304c9e4961b62303bdcffdd6a06d
Caleb Christensen <Caleb.Christ@gmail.com>
2018-10-24 13:15:25 -0400

    ts files for 3 fetches

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { InputsComponent } from './inputs/inputs.component';
-import { PeopleComponent } from './people/people.component';
-import { ShipsComponent } from './ships/ships.component';
-import { FilmsComponent } from './films/films.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     InputsComponent,
-    PeopleComponent,
-    ShipsComponent,
-    FilmsComponent
   ],
   imports: [
     BrowserModule
